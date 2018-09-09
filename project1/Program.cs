f7066aabeeeeb7334a5f67e6765a3822cf8660b3
Ryan Thomas <14257080+rtnmn74@users.noreply.github.com>
2018-09-09 17:24:40 -0500

    Updated program

--- project1/Program.cs
+++ project1/Program.cs
@@ -7,6 +7,7 @@ namespace project1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey there");
         }
     }
 }
