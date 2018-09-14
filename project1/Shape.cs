e89d3676f0200806fa7469306dd7379a381f6fb4
Ryan Thomas <14257080+rtnmn74@users.noreply.github.com>
2018-09-14 15:59:38 -0500

    add shape

---
+++ project1/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Project1
+{
+    class Shape
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Width: ");
+            string userInputWidth = Console.ReadLine(); 
+            Console.WriteLine("Enter Length: ");
+            string userInputLength = Console.ReadLine();
+            int width = Convert.ToInt32(userInputWidth);
+            int length = Convert.ToInt32(userInputLength);
+            int perimeter = width + width + length + length;
+            Console.WriteLine();
+            Console.WriteLine("The perimeter is {0}", perimeter);
+        }
+    }
+}
