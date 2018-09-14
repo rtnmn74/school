b37acb781bb31d90bf3ad63810fe2028370207a8
Ryan Thomas <14257080+rtnmn74@users.noreply.github.com>
2018-09-14 16:55:13 -0500

    fix shape

--- project1/Shape.cs
+++ project1/Shape.cs
@@ -14,15 +14,19 @@ namespace Project1
 
             /*Take user input for shape width */
             Console.Write("Enter Width: ");
-            userInputWidth = Console.ReadLine(); 
+            userInputWidth = Console.ReadLine();
+            /*Convert user input string to integer for calculating */
+            width = Convert.ToInt32(userInputWidth);
+            /*Output user input for width */
+            Console.WriteLine("You Entered {0}", width); 
 
-           /*Take user inpout for shape lendth */
+           /*Take user inpout for shape length */
             Console.Write("Enter Length: ");
             userInputLength = Console.ReadLine();
-
             /*Convert user input strings to integers for calculating */
-            width = Convert.ToInt32(userInputWidth);
             length = Convert.ToInt32(userInputLength);
+            /*Output user input length*/
+            Console.WriteLine("You entered {0}", length);
 
             /*Calculate the perimeter of the shape length and width inputs */
             perimeter = width + width + length + length;
