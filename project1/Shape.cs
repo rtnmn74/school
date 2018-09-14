6dfecd239dae973233cc396cf9395f35f7d52c46
Ryan Thomas <14257080+rtnmn74@users.noreply.github.com>
2018-09-14 16:26:01 -0500

    add comments

--- project1/Shape.cs
+++ project1/Shape.cs
@@ -12,18 +12,23 @@ namespace Project1
             int length;
             int perimeter;
 
-
+            /*Take user input for shape width */
             Console.Write("Enter Width: ");
             userInputWidth = Console.ReadLine(); 
-           
+
+           /*Take user inpout for shape lendth */
             Console.Write("Enter Length: ");
             userInputLength = Console.ReadLine();
 
+            /*Convert user input strings to integers for calculating */
             width = Convert.ToInt32(userInputWidth);
             length = Convert.ToInt32(userInputLength);
+
+            /*Calculate the perimeter of the shape length and width inputs */
             perimeter = width + width + length + length;
-         
-            Console.Write("The perimeter is {0}", perimeter);
+            
+            /* Output the perimeter of the shape based on the inputs */
+            Console.WriteLine("The perimeter is {0}", perimeter);
         }
     }
 }
