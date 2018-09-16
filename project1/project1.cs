f59a4b6492ee24a91dfaa736373667404eb1c670
Ryan Thomas <14257080+rtnmn74@users.noreply.github.com>
2018-09-16 15:47:49 -0500

    update repo

---
+++ project1/project1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Project1
+{
+    class Rectangle1 
+    {
+        int shapeWidth; 
+        int shapeHeight;
+
+        public Rectangle1(int width, int height)
+        {
+            shapeWidth = width;
+            shapeHeight = height;
+        } 
+        public int area()
+        {
+        return shapeWidth * shapeHeight;
+        }
+        public int perimeter()
+        {
+         return shapeWidth + shapeWidth + shapeHeight + shapeHeight;
+        }
+   
+    class Square1 : Rectangle1
+    {
+        public Square1(int width, int height) : base (width, height)
+        {
+        area();
+        perimeter();
+        }
+    }   
+
+    static void Test ( string[ ] args )
+    {
+       //* test square */
+        Square1 square = new Square1(50, 50);
+        Console.WriteLine ("The area of the square is {0}", square.area());
+        Console.WriteLine ("The perimater of the square is {0}", square.perimeter());
+
+        //* test rectangle */
+        Rectangle1 rectangle = new Rectangle1(150, 50);
+        Console.WriteLine ("The area of the rectangle is {0}", rectangle.area());
+        Console.WriteLine ("The perimater of the rectangle is {0}", rectangle.perimeter());
+        
+
+         
+    
+    }
+   }
+
+}
