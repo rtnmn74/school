f4c39dcb2fd246407e9c0cdd8d0091dba8d0075c
Ryan Thomas <14257080+rtnmn74@users.noreply.github.com>
2018-09-17 06:46:35 -0500

    add unit tests

--- project1/Program.cs
+++ project1/Program.cs
@@ -7,55 +7,97 @@ namespace Project1
     {
         int width; 
         int height;
-        int area;
-        int perimeter;
 
-        public Rectangle1(int inputWidth, int inputHeight)
+        public int computeArea(int width, int height)
         {
-            width = inputWidth;
-            height = inputHeight;
-            computeArea();
-            computePerimeter();
-        } 
-        public int computeArea()
-        {
-        return area = width * height;
+        return width * height;
         }
-        public int computePerimeter()
+        public int computePerimeter(int width, int height)
         {
-         return perimeter = width + width + height + height;
+         return width + width + height + height;
+        }
+        public Rectangle1()
+        {
+            computeArea(width, height);
+            computePerimeter(width, height);
         }
    
     class Square1 : Rectangle1
     {
-        public Square1(int inputWidth, int inputHeight) : base (inputWidth, inputHeight)
+        public Square1()
         {
-            width = inputWidth;
-            height = inputHeight; 
-            computeArea();
-            computePerimeter();
+            computeArea(width, height);
+            computePerimeter(width, height);
         }
     }   
 
 [TestFixture]
-class Test
-{
-       [TestCase]
-       public void testSquare()
-       {
-        Square1 square = new Square1(50, 50);
-        Assert.That(square.area, Is.EqualTo(2500));
-        Assert.That(square.perimeter, Is.EqualTo(200));
-       }
-       [TestCase]
-        public void testRectangle()
+class RectangleAreaTest
+    {
+        [Test]
+        public void testRectangleArea()
         {
-        Rectangle1 rectangle = new Rectangle1(150, 50);
-        Assert.That(rectangle.area, Is.EqualTo(7500));
-        Assert.That(rectangle.perimeter, Is.EqualTo(400));
+        /* Given new rectangle compute the area */
+        var rectangle = new Rectangle1();
+
+        /* Compute the area of 150 and 50 */
+        var returnValue = rectangle.computeArea(150,50);
+
+        /* The area of 150 and 50 should be 7500 */
+        Assert.That(returnValue, Is.EqualTo(7500));
         }
+
+class RectanglePerimeterTest
+    {
+        [Test]
+        public void testRectanglePerimeter()
+        {
+        /* Given new rectangle compute the perimeter */
+        var rectangle = new Rectangle1();
+
+        /* Compute the perimter of 150 and 50 */
+        var returnValue = rectangle.computePerimeter(150,50);
+
+        /* The perimeter of 150 and 50 should be 400 */
+        Assert.That(returnValue, Is.EqualTo(400));
+        }
+
+     }
+     class SquareAreaTest
+    {
+        [Test]
+        public void testSquareArea()
+        {
+        /* Given new square compute the area */
+        var square = new Square1();
+
+        /* Compute the area of 50 and 50 */
+        var returnValue = square.computeArea(50,50);
+
+        /* The area of 50 and 50 should be 2000 */
+        Assert.That(returnValue, Is.EqualTo(2500));
+        }
+
+class SquarePerimeterTest
+    {
+        [Test]
+        public void testSquarePerimeter()
+        {
+        /* Given new square compute the perimeter */
+        var square = new Square1();
+
+        /* Compute the perimter of 50 and 50 */
+        var returnValue = square.computePerimeter(50,50);
+
+        /* The perimeter of 50 and 50 should be 200 */
+        Assert.That(returnValue, Is.EqualTo(200));
+        }
+
+     }
+
+
 }
-    
+    }
     
    }
 
