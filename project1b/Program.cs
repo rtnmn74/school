969f00b503984c60e7ecf78f8ff85426821d4531
Ryan Thomas <14257080+rtnmn74@users.noreply.github.com>
2018-09-20 06:17:25 -0500

    update folders

--- project1b/Program.cs
+++ project1b/Program.cs
@@ -2,77 +2,77 @@
 using NUnit.Framework;
 
 namespace Project1 {
-    /* Class for Rectangle1 */
-    class Rectangle1 {
+    /* Class for Square2 */
+    class Square2 {
         /* Parameters for a shape with 4 sides */
         public int width;
         public int length;
 
-        /*Contructor for a rectangle */
-        public Rectangle1 (int width, int length) {
+        /*Contructor for a square */
+        public Square2 (int width, int length) {
             this.width = width;
             this.length = length;
         }
 
-        /* Routine that computes the area of a rectangle  */
+        /* Routine that computes the area of a square  */
         public virtual int computeArea () {
-            /* Return area of the rectangle if width and length are not equal */
+            /* Return area of the square if width and length are not equal */
             /* Throw an exception if width and length are equal */
-            if (width == length) {
-                throw new System.ArgumentException ("Rectangle width and length values cannot match", "rectangle.width and rectangle.length");
+            if (width != length) {
+                throw new System.ArgumentException ("Square width and length values must match", "square.width and square.length");
             } else
-            /* Return the area of a rectangle by multiplying the width x the length */
+            /* Return the area of a square by multiplying the width x the length */
             {
                 return width * length;
             }
         }
 
-        /* Routine that computes the perimeter of a rectangle */
+        /* Routine that computes the perimeter of a square */
         public virtual int computePerimeter ()
-        /* Return perimeter of the rectangle if the width and length are not equal */
+        /* Return perimeter of the square if the width and length are not equal */
         /* Throw and exception if the width and length are equal */
         {
-            if (width == length) {
-                throw new System.ArgumentException ("Rectangle width and length values cannot match", "rectangle.width and rectangle.length");
+            if (width != length) {
+                throw new System.ArgumentException ("Square width and length values must match", "square.width and square.length");
             } else
 
             {
-                /* Return the perimeter of the rectangle by adding 2 times the width with 2 times the length  */
+                /* Return the perimeter of the square by adding 2 times the width with 2 times the length  */
                 return width + width + length + length;
             }
         }
 
     }
-    /* Subclass for Square1 */
-    class Square1 : Rectangle1 {
-        /* Contructor for a square */
-        public Square1 (int width, int length) : base (width, length) {
+    /* Subclass for Rectangle2 */
+    class Rectangle2 : Square2 {
+        /* Contructor for a rectangle */
+        public Rectangle2 (int width, int length) : base (width, length) {
             this.width = width;
             this.length = length;
         }
 
-        /* Routine that computes the area of a square */
+        /* Routine that computes the area of a rectangle */
         public override int computeArea ()
-        /* Return arear of the square if the width and length are equal */
+        /* Return arear of the rectangle if the width and length are equal */
         /* Throw and exception if the width and length are not equal */
         {
-            if (width != length) {
-                throw new System.ArgumentException ("Square width and length values must match", "square.width and square.length");
+            if (width == length) {
+                throw new System.ArgumentException ("Rectangle width and length values cannot match", "rectangle.width and rectangle.length");
             } else
-            /* Return the area of a square by multplying the width x the length */
+            /* Return the area of a rectangle by multplying the width x the length */
             {
                 return width * length;
             }
 
         }
-        /* Routine that computes the area of a square */
+        /* Routine that computes the area of a rectangle */
         public override int computePerimeter () {
-            /* Return arear of the square if the width and length are equal */
+            /* Return arear of the rectangle if the width and length are equal */
             /* Throw and exception if the width and length are not equal */
-            if (width != length) {
-                throw new System.ArgumentException ("Square width and length values must match", "square.width and square.length");
+            if (width == length) {
+                throw new System.ArgumentException ("Rectangle width and length values cannot match", "rectangle.width and rectangle.length");
             } else
-            /* Return the perimeter of a sqaure by adding the 2 times the width with 2 times the length */
+            /* Return the perimeter of a rectangle by adding the 2 times the width with 2 times the length */
             {
                 return width + width + length + length;
             }
@@ -86,7 +86,7 @@ namespace Project1 {
         [Test]
         public void testRectangleArea () {
             /* Given new rectangle with a width of 150 and a length of 50, compute the area */
-            var rectangle = new Rectangle1 (150, 50);
+            var rectangle = new Rectangle2 (150, 50);
 
             /* The area calculation of 150 and 50 should be 7500 */
             Assert.That (rectangle.computeArea, Is.EqualTo (7500));
@@ -96,7 +96,7 @@ namespace Project1 {
             [Test]
             public void testRectanglePerimeter () {
                 /* Given new rectangle with a width of 150 and a length of 50, compute the perimeter */
-                var rectangle = new Rectangle1 (150, 50);
+                var rectangle = new Rectangle2 (150, 50);
 
                 /* The perimeter calculation of 150 and 50 should be 400 */
                 Assert.That (rectangle.computePerimeter, Is.EqualTo (400));
@@ -107,7 +107,7 @@ namespace Project1 {
             [Test]
             public void testSquareArea () {
                 /* Given a new square with a width of 50 and a length of 50, compute the area */
-                var square = new Square1 (50, 50);
+                var square = new Square2 (50, 50);
 
                 /* The area calculation of 50 and 50 should be 2500 */
                 Assert.That (square.computeArea, Is.EqualTo (2500));
@@ -117,7 +117,7 @@ namespace Project1 {
                 [Test]
                 public void testSquarePerimeter () {
                     /* Given new square with a width of 50 and a length of 50, compute the perimeter */
-                    var square = new Square1 (50, 50);
+                    var square = new Square2 (50, 50);
 
                     /* The perimeter calculation of 50 and 50 should be 200 */
                     Assert.That (square.computePerimeter, Is.EqualTo (200));
@@ -129,7 +129,7 @@ namespace Project1 {
                 [Test]
                 public void testRectangleAreaException () {
                     /* Given new rectangle with a width of 50 and a length of 50, compute the area */
-                    var rectangle = new Rectangle1 (50, 50);
+                    var rectangle = new Rectangle2 (50, 50);
 
                     /* The area calcualtion of 50 and 50 should throw an exception */
                     var ex = Assert.Throws<System.ArgumentException> (() => rectangle.computeArea ());
@@ -141,7 +141,7 @@ namespace Project1 {
                 [Test]
                 public void testRectanglePerimeterException () {
                     /* Given new rectangle with a width of 50 and a length of 50, compute the perimeter */
-                    var rectangle = new Rectangle1 (50, 50);
+                    var rectangle = new Rectangle2 (50, 50);
 
                     /* The perimeter calcualtion of 50 and 50 should throw an exception */
                     var ex = Assert.Throws<System.ArgumentException> (() => rectangle.computePerimeter ());
@@ -154,7 +154,7 @@ namespace Project1 {
                 [Test]
                 public void testSquareAreaException () {
                     /* Given new square with a width of 51 and a length of 50, compute the area */
-                    var square = new Square1 (51, 50);
+                    var square = new Square2 (51, 50);
 
                     /* The area calcualtion of 51 and 50 should throw an exception */
                     var ex = Assert.Throws<System.ArgumentException> (() => square.computeArea ());
@@ -166,7 +166,7 @@ namespace Project1 {
                 [Test]
                 public void testSquarePerimeterException () {
                     /* Given new square with a width of 51 and a length of 50, compute the perimeter */
-                    var square = new Square1 (51, 50);
+                    var square = new Square2 (51, 50);
 
                     /* The perimeter calcualtion of 51 and 50 should throw an exception */
                     var ex = Assert.Throws<System.ArgumentException> (() => square.computePerimeter ());
