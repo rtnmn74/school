4b2194c18c2d94f53eb7c8bd5189b408c4a70e57
Ryan Thomas <14257080+rtnmn74@users.noreply.github.com>
2018-10-04 07:02:38 -0500

    new back account program

---
+++ assignment3/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace assignment3 {
+    class BankAccount {
+        private int accountId;
+        private double balance;
+
+        public void deposit () {
+
+           
+        }
+
+        public void withdraw () {
+
+        }
+
+        public void computeBalance () {
+
+            balance = deposit - withdraw;
+
+        }
+
+
+        public void clone () {
+
+            public void equals () {
+
+            }
+
+        }
+
+        public BankAccount () {
+
+        }
+
+    }
+}
--- assignment3/Program.cs
+++ assignment3/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
+using System;
 
-namespace assignment3
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace assignment3 {
+    class Program {
+        static void Main (string[] args) {
+            var account = new BankAccount ("John Smith", 1000);
+            Console.WriteLine ($"Account {account.AccountNumber} was created for {account.AccountHolder} with {account.Balance} opening balance.");
         }
     }
 }
