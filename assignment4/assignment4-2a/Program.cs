76be5c296c5f9233941264a2203d36f140d9e9e9
Ryan Thomas <14257080+rtnmn74@users.noreply.github.com>
2018-10-28 10:02:36 -0500

    add assignment 4

---
+++ assignment4/assignment4-2a/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Program {
+
+    class Program {
+
+        static void Main (string[] args) {
+            Program program = new Program ();
+            program.Item = Enumerable.Range (1, 111111500).ToArray();
+            program.find (1123451);
+        }
+
+        int[] item;
+        public int[] Item { get; set; }
+
+        public bool find (int testValue) {
+
+            Stopwatch stopwatch = new Stopwatch ();
+            stopwatch.Start ();
+
+            bool found = false;
+            int i = 0;
+            while ((!found) && (i < Item.Length)) {
+                if (Item[i] == testValue)
+                    found = true;
+                else
+                    i++;
+            }
+
+            stopwatch.Stop ();
+            Console.WriteLine ("Time elapsed: {0}", stopwatch.ElapsedMilliseconds);
+
+            return found;
+
+        }
+
+    }
+
+}
---
+++ assignment4/assignment4-2b/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Program {
+
+    class Program {
+
+        static void Main (string[] args) {
+            Program program = new Program ();
+            program.Item = Enumerable.Range (1, 111111500).ToArray();
+            program.find (1123451);
+        }
+
+        int[] item;
+        public int[] Item { get; set; }
+
+        public bool find (int testValue) {
+
+            Stopwatch stopwatch = new Stopwatch ();
+            stopwatch.Start ();
+
+            int intialValue = Item[Item.Length - 1];
+            Item[Item.Length - 1] = testValue;
+            int i = 0;
+            while (Item[i] != testValue) {
+                i++;
+            }
+            Item[Item.Length - 1] = intialValue;
+
+            stopwatch.Stop ();
+            Console.WriteLine("Time elapsed: {0}", stopwatch.ElapsedMilliseconds);
+
+            return i < Item.Length - 1 || testValue == intialValue;
+
+        }
+
+    }
+
+}
