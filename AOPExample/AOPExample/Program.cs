1432814a82ed0547bfe89ddd996af2e56330697e
Ryan Thomas <14257080+rtnmn74@users.noreply.github.com>
2018-11-10 14:21:49 -0600

    Add example

---
+++ AOPExample/AOPExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AOPExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Hello.writeHello());
+            System.Console.WriteLine(World.writeWorld());
+        }
+    }
+}
