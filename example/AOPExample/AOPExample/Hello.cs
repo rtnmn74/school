5af0349cef125658348f4c7f3b0f5be5806804fb
Ryan Thomas <14257080+rtnmn74@users.noreply.github.com>
2018-11-10 14:18:06 -0600

    Add AOP example

---
+++ example/AOPExample/AOPExample/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace AOPExample
+{
+    class Hello
+    {
+        [LogExecutionTimeAttribute]
+        public static string writeHello()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return "Hello";
+        }
+    }
+}
